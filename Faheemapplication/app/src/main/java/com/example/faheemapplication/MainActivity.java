fd9212fbd1e7b17840da5ee4ed326cb209e59631
shhadr <shahadalmulhiim@gmail.com>
2021-01-26 16:50:41 +0300

    First commit

---
+++ Faheemapplication/app/src/main/java/com/example/faheemapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.faheemapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_DELAY = 3500; // The time that the splash screen will take to disappear
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide(); // This will hide the action bar in the screen
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+
+}
---
+++ Faheemapplication/app/src/main/java/com/example/faheemapplication/MainScreen.java
@@ -0,0 +1,15 @@
+package com.example.faheemapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        getSupportActionBar().hide();
+    }
+}
